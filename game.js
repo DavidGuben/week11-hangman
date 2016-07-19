40d3900a7d45b8990d950f9263c0436a1d8696c2
DavidGuben <davidjg6@gmail.com>
2016-07-18 23:20:37 -0400

    adding packages and working hangman app

--- game.js
+++ game.js
@@ -1,1 +1,5 @@
 // Your game.js file will randomly select a word for the player.
+
+exports.game = {
+	wordBank : ["heart and soul", "relax", "burning up", "manic monday", "into the groove", "invisible touch", "rebel yell"]
+};
